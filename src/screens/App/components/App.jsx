d1c86abf442650ab13fcc3dd28adebbde4ffe6e4
gn-grc <genejogarcia.gg@gmail.com>
2021-08-11 22:37:58 +0800

    renames

--- src/screens/App/components/App.jsx
+++ src/screens/App/components/App.jsx
@@ -7,11 +7,13 @@ import route from "../route";
 function App() {
   return (
     <Router>
-      <h1>Welcome to the App</h1>
+      {/* <h1>Welcome to the App</h1> */}
 
       <Switch>
         <Route {...route.SIGN_UP} />
         <Route {...route.SIGN_IN} />
+
+        {/* <ProtectedRoute ...route /> */}
       </Switch>
     </Router>
   );
--- src/screens/App/route.js
+++ src/screens/App/route.js
@@ -1,8 +1,8 @@
 import routes from "../../shared/routes";
 
 // components' route
-import SignUpRoute from "./screens/SIGN_UP/route";
-import SignInRoute from "./screens/SIGN_IN/route";
+import SignUpRoute from "./screens/SignUp/route";
+import SignInRoute from "./screens/SignIn/route";
 
 export default {
   SIGN_UP: SignUpRoute,
