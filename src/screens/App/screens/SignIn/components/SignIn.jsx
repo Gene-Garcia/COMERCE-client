c98b20e1c0cd7a2a2827465bdd362a1693cecc29
gn-grc <genejogarcia.gg@gmail.com>
2021-08-12 22:00:52 +0800

    added design for sign in and sign up

--- src/screens/App/screens/SignIn/components/SignIn.jsx
+++ src/screens/App/screens/SignIn/components/SignIn.jsx
@@ -1,7 +1,33 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
+import "./SignIn.css";
 
 function SignIn() {
-  return <h1>SignIn</h1>;
+  return (
+    <div id="signIn" className="page-content">
+      <div className="only-content">
+        <div>
+          <h2>Sign In</h2>
+        </div>
+
+        <div>
+          <input type="email" className="input" placeholder="Email" />
+        </div>
+
+        <div>
+          <input type="password" className="input" placeholder="Password" />
+        </div>
+
+        <div>
+          <input type="submit" className="submit-form" value="Login" />
+          <Link to="/sign-up" className="sign-up-link">
+            Don't have an account yet
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default SignIn;
--- src/screens/App/screens/SignUp/components/SignUp.jsx
+++ src/screens/App/screens/SignUp/components/SignUp.jsx
@@ -1,7 +1,37 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
+import "./SignUp.css";
 
 function SignUp() {
-  return <h1>SignUp</h1>;
+  return (
+    <div id="signUp" className="page-content">
+      <div className="only-content">
+        <div>
+          <h2>Sign Up</h2>
+        </div>
+
+        <div>
+          <input type="email" className="input" placeholder="Email" />
+        </div>
+
+        <div>
+          <input type="text" className="input" placeholder="Username" />
+        </div>
+
+        <div>
+          <input type="password" className="input" placeholder="Password" />
+        </div>
+
+        <div>
+          <input type="submit" className="submit-form" value="Create Account" />
+          <Link to="/sign-in" className="sign-in-link">
+            Already have an account
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default SignUp;
