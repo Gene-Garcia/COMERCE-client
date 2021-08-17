f7fe6e53e16801228ba4f9b6293e7cb2a5342520
gn-grc <genejogarcia.gg@gmail.com>
2021-08-17 11:47:54 +0800

    extended customized colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@ module.exports = {
   purge: [],
   darkMode: false, // other values 'media' and 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "my-accent": "#0092CA",
+        "my-contrast": "#F9FAFB",
+        "my-black": "#082032",
+        "my-dark": "#2C394B",
+        "my-dim": "#334756",
+      },
+    },
   },
   variants: {
     extend: {},
