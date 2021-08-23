257e56a57fd18aae55304d138508b58bffa8d0d6
gn-grc <genejogarcia.gg@gmail.com>
2021-08-23 22:39:05 +0800

    created a reusable gradient title page

---
+++ src/shared/Components/pages/Title.Page.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Title(title) {
+  return (
+    <div className="h-20 shadow-md bg-gradient-to-t from-my-accent to-my-accent-mono flex items-center justify-center">
+      <span className="font-sans font-semibold text-my-contrast text-xl">
+        Product Catalogue
+      </span>
+    </div>
+  );
+}
+export default Title;
