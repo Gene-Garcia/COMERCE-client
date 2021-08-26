33eff4b618ca50ffd09f8eab1ab24247d32cbec9
gn-grc <genejogarcia.gg@gmail.com>
2021-08-26 22:20:27 +0800

    created a custom hook that returns the state of the alert context

---
+++ src/hooks/useAlert.js
@@ -0,0 +1,10 @@
+import React, { useContext } from "react";
+import AlertContext from "../context/AlertContext";
+
+function useAlert() {
+  const { message, setMessage, severity, setSeverity } =
+    useContext(AlertContext);
+
+  return { message, setMessage, severity, setSeverity };
+}
+export default useAlert;
