1a94ed93645bb4b835974a4dec62cb5df71c6e94
gn-grc <genejogarcia.gg@gmail.com>
2021-08-30 17:43:57 +0800

    initial product showcase layout

--- src/screens/App/screens/Catalogue/screens/ProductShowcase/components/Showcase.jsx
+++ src/screens/App/screens/Catalogue/screens/ProductShowcase/components/Showcase.jsx
@@ -1,8 +1,56 @@
 import React from "react";
+import {
+  ProductDescription,
+  ProductPrice,
+  ProductPurchase,
+  ProductRating,
+} from "../../../../../../../shared/Components/product/ProductComponents";
 
 function Showcase(props) {
   console.log(props);
-  return <h1>Showcase/:id</h1>;
+  return (
+    <>
+      <div className="h-3/5 bg-gradient-to-b from-my-accent">
+        <h1 className="text-4xl font-semibold text-my-contrast text-center font-sans pt-12">
+          Smart Watch
+        </h1>
+      </div>
+
+      <div className="flex container mx-auto gap-x-12 -mt-106 mb-16 ">
+        {/* image */}
+        <div className="w-3/5">
+          <img
+            src="https://img.endgamegear.com/products/xm1-rgb/2"
+            alt="smart-watch"
+            className=""
+          />
+        </div>
+
+        {/* content */}
+        <div className="w-2/5 bg-white rounded-lg shadow-lg p-16 flex flex-col gap-y-20">
+          {/* buy now */}
+          <div>
+            <p className="text-gray-500 font-medium text-2xl">
+              Buy now for only
+            </p>
+            <ProductPrice price="13,999.99" />
+            <br />
+            <ProductPurchase />
+          </div>
+
+          {/* rating */}
+          <div>
+            <ProductRating />
+          </div>
+
+          {/* desc */}
+          <div>
+            <ProductDescription desc="lorem upsum" />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Showcase;
