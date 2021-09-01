c6624b16e199c27d6d88f9328a7f35e3e752dab1
gn-grc <genejogarcia.gg@gmail.com>
2021-09-01 23:35:25 +0800

    home nav tweaks

--- src/screens/App/screens/Home/components/HomeNav.jsx
+++ src/screens/App/screens/Home/components/HomeNav.jsx
@@ -7,12 +7,12 @@ import Account from "../../../../../shared/Components/navigation/Account";
 
 function HomeNav() {
   return (
-    <div className="flex justify-between flex-wrap w-full bg-gradient-to-t from-my-accent to-my-accent-mono px-16">
+    <div className="flex flex-col md:flex-row justify-between w-full bg-gradient-to-t from-my-accent to-my-accent-mono px-3 sm:px-10 lg:px-16 gap-x-10 md:gap-x-10  xl:gap-x-40">
       {/* logo */}
-      <div className="mx-auto md:mx-0 py-16 ">
-        <div className="inline-flex space-x-8">
+      <div className="flex items-center mx-auto md:mx-0 py-4 md:py-16 ">
+        <div className="flex flex-row gap-x-3 lg:gap-x-8">
           <div>
-            <img src={iconLogo} alt="comerce-logo" width="180" />
+            <img src={iconLogo} alt="comerce-logo" className="w-full" />
           </div>
           <div className="self-center font-mono font-bold text-4xl tracking-widest text-my-contrast">
             COMERCE
@@ -21,31 +21,28 @@ function HomeNav() {
       </div>
 
       {/* links actions */}
-      <div className="py-9 ">
-        <div className="flex justify-end items-center space-x-6">
-          {/* catalogue link */}
-          <div className="flex-shrink place-self-center">
+      <div className="py-9 flex-grow">
+        <div className="flex flex-col xl:flex-row gap-x-6 gap-y-6">
+          {/* search & catalogue link */}
+          <div className="flex-grow flex flex-col">
+            <Search />
             <Link
               to="/catalogue"
-              className="font-sans text-lg mb-0 font-semibold text-my-contrast transition ml-8 sm:ml-0 duration-300 border-opacity-50 hover:border-opacity-50 hover:border-b hover:border-my-contrast"
+              className="self-end transition duration-100 mt-0.5 font-sans text-base font-semibold text-gray-100"
             >
               Product Catalogue
             </Link>
           </div>
 
-          {/* search */}
-          <div className="flex-shrink-0">
-            <Search />
-          </div>
-
           {/* account */}
-          <div className="flex-shrink-0">
-            <Account bgType="accent" />
-          </div>
-
-          {/* cart */}
-          <div className="">
-            <Cart bgType="accent" />
+          <div className="self-end xl:place-self-end flex flex-row items-center gap-x-6 ">
+            <div className="">
+              <Account bgType="accent" />
+            </div>
+            {/* cart */}
+            <div className="">
+              <Cart bgType="accent" />
+            </div>
           </div>
         </div>
       </div>
