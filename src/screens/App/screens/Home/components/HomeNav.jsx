31f6f9a4a7f1f56732cc3f26b8761f5b48690bf1
gn-grc <genejogarcia.gg@gmail.com>
2021-09-01 23:56:50 +0800

    improved responsiveness of home nav

--- src/screens/App/screens/Home/components/HomeNav.jsx
+++ src/screens/App/screens/Home/components/HomeNav.jsx
@@ -7,22 +7,22 @@ import Account from "../../../../../shared/Components/navigation/Account";
 
 function HomeNav() {
   return (
-    <div className="flex flex-col md:flex-row justify-between w-full bg-gradient-to-t from-my-accent to-my-accent-mono px-3 sm:px-10 lg:px-16 gap-x-10 md:gap-x-10  xl:gap-x-40">
+    <div className="flex flex-col md:flex-row justify-between w-full bg-gradient-to-t from-my-accent to-my-accent-mono py-3 md:py-0 px-3 sm:px-10 lg:px-16 gap-y-3 md:gap-x-10 2xl:gap-x-80">
       {/* logo */}
-      <div className="flex items-center mx-auto md:mx-0 py-4 md:py-16 ">
-        <div className="flex flex-row gap-x-3 lg:gap-x-8">
-          <div>
-            <img src={iconLogo} alt="comerce-logo" className="w-full" />
+      <div className="flex items-center mx-auto md:mx-0 py-0 sm:py-4 md:py-16 ">
+        <div className="flex flex-row justify-center md:justify-start gap-x-5">
+          <div className="w-1/5 md:w-1/3 lg:w-2/4 xl:3/4 2xl:w-full ">
+            <img src={iconLogo} alt="comerce-logo" className="" />
           </div>
-          <div className="self-center font-mono font-bold text-4xl tracking-widest text-my-contrast">
+          <div className="self-center font-mono font-bold text-2xl sm:text-4xl tracking-widest text-my-contrast">
             COMERCE
           </div>
         </div>
       </div>
 
       {/* links actions */}
-      <div className="py-9 flex-grow">
-        <div className="flex flex-col xl:flex-row gap-x-6 gap-y-6">
+      <div className="w-full py-0 sm:py-3 md:py-9 flex-col">
+        <div className="flex flex-col xl:flex-row gap-x-6 gap-y-4 md:gap-y-6">
           {/* search & catalogue link */}
           <div className="flex-grow flex flex-col">
             <Search />
