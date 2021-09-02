e40aa9d3de7da17bd7a61486720bf6827dbaf6d1
gn-grc <genejogarcia.gg@gmail.com>
2021-09-02 11:32:08 +0800

    relocated alert notifiction to top center

--- src/shared/Components/pages/Alert.jsx
+++ src/shared/Components/pages/Alert.jsx
@@ -10,34 +10,38 @@ function Alert() {
 
   return (
     <div
-      className={
-        (message ? "absolute" : "hidden") +
-        " transition duration-300 py-6 w-80 rounded-md shadow-md border-l-4 px-4 flex justify-between items-center left-8 bottom-8 " +
-        color
-      }
+      className={(message ? "absolute" : "hidden") + " top-32 left-1/2 z-10"}
     >
-      <div className="text-gray-800 font-semibold">{message}</div>
-
-      <button
-        onClick={() => {
-          setMessage("");
-        }}
+      <div
+        className={
+          (message ? "relative" : "hidden") +
+          " transition duration-300 py-4 rounded-md shadow-md border-l-4 gap-x-6 px-3 flex justify-between items-center mx-auto -left-1/2 " +
+          color
+        }
       >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="h-6 w-6"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
+        <div className="text-gray-600 font-semibold pr-4">{message}</div>
+
+        <button
+          onClick={() => {
+            setMessage("");
+          }}
         >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth={2}
-            d="M6 18L18 6M6 6l12 12"
-          />
-        </svg>
-      </button>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-5 w-5"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M6 18L18 6M6 6l12 12"
+            />
+          </svg>
+        </button>
+      </div>
     </div>
   );
 }
