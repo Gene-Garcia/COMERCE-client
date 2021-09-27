de6171c340b7550562f4d79022f1aa549f2ce1d6
gn-grc <genejogarcia.gg@gmail.com>
2021-09-27 16:28:05 +0800

    added initial template layout for order links

---
+++ src/screens/App/screens/User/screens/Orders/components/OrderLinks.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+
+function OrderLinks() {
+  return (
+    <>
+      <div className="flex flex-row items-center gap-6 mb-8">
+        <p className="font-semibold text-xl">Orders</p>
+        <p className="font-medium text-gray-500">Select to view details</p>
+      </div>
+
+      {/* link buttons */}
+      <div className="flex flex-col gap-y-6">
+        <OrderLink />
+        <OrderLink />
+        <OrderLink />
+        <OrderLink />
+        <OrderLink />
+      </div>
+    </>
+  );
+}
+export default OrderLinks;
+
+function OrderLink() {
+  return (
+    <div
+      className="group flex flex-row items-center justify-between"
+      onClick={() => alert("Clicked")}
+    >
+      {/* id and date */}
+      <div>
+        <p className="transition duration-300 font-medium text-lg text-gray-700 group-hover:text-my-accent group-hover:underline">
+          61330ffda979f0da4b4a3f4
+        </p>
+        <p className="font-medium text-md text-gray-400">SEP 18 2021</p>
+      </div>
+
+      {/* arrow chevron */}
+      <div>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="transition duration-300 h-5 w-5 group-hover:text-my-accent"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={2}
+            d="M9 5l7 7-7 7"
+          />
+        </svg>
+      </div>
+    </div>
+  );
+}
