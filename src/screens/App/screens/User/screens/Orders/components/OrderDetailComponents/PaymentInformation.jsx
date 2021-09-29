7349395f491342b15fdabc4fc5fe000413bfcd23
gn-grc <genejogarcia.gg@gmail.com>
2021-09-29 23:20:37 +0800

    implemented populating payment information with computation

--- src/screens/App/screens/User/screens/Orders/components/OrderDetailComponents/PaymentInformation.jsx
+++ src/screens/App/screens/User/screens/Orders/components/OrderDetailComponents/PaymentInformation.jsx
@@ -1,36 +1,61 @@
 import React from "react";
+import useOrders from "../../../../../../../../hooks/useOrders";
+import {
+  displayPaymentInfo,
+  methods as pMethods,
+} from "../../../../../../../../shared/utils/payment";
 import InformationBody from "./InformationBody";
 
 function PaymentInformation() {
+  const { order, loading, computeSubTotal } = useOrders();
+
   return (
     <>
-      <div className="flex flex-row gap-24 mb-6">
-        {/* subtotal */}
-        <div>
-          <InformationBody title="Subtotal" value="P17,680.00" />
-        </div>
-        {/* shipping fee */}
-        <div>
-          <InformationBody title="Shipping Fee" value="P70.00" />
-        </div>
+      {loading || !order ? (
+        <></>
+      ) : (
+        <>
+          <div className="flex flex-row gap-24 mb-6">
+            {/* subtotal */}
+            <div>
+              <InformationBody
+                title="Subtotal"
+                value={`P${computeSubTotal()}`}
+              />
+            </div>
+            {/* shipping fee */}
+            <div>
+              <InformationBody
+                title="Shipping Fee"
+                value={`P${order.shippingFee}`}
+              />
+            </div>
 
-        {/* grand total */}
-        <div>
-          <InformationBody
-            title="Grand Total after shipping fee"
-            value="P17,750.00"
-          />
-        </div>
-      </div>
+            {/* grand total */}
+            <div>
+              <InformationBody
+                title="Grand Total after shipping fee"
+                value={`P${order.shippingFee + computeSubTotal()}`}
+              />
+            </div>
+          </div>
 
-      <div>
-        <p className="font-medium text-gray-800 text-lg">
-          <span className="text-my-accent">Payment Method</span> payment
-        </p>
-        <p className="font-medium text-gray-800 text-lg">
-          9887 7898 7985 | johndoe@paypal.com
-        </p>
-      </div>
+          <div>
+            <p className="font-medium text-gray-800 text-opacity-80 text-lg px-2">
+              <span className="text-my-accent text-opacity-80">
+                {pMethods[order.paymentMethod]}
+              </span>{" "}
+              payment
+            </p>
+            <p className="font-medium text-gray-800 text-lg bg-gray-100 rounded-md shadow-sm px-2 py-.05">
+              {displayPaymentInfo(
+                order.paymentMethod,
+                order.paymentInformation
+              )}
+            </p>
+          </div>
+        </>
+      )}
     </>
   );
 }
