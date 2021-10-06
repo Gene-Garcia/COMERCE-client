18a748626f60edc449789901a8a7a1ea7a830d44
gn-grc <genejogarcia.gg@gmail.com>
2021-10-06 23:10:38 +0800

    implemented rendering product card links

--- src/screens/App/screens/User/screens/Orders/screens/Rate/components/ProductLinks.jsx
+++ src/screens/App/screens/User/screens/Orders/screens/Rate/components/ProductLinks.jsx
@@ -1,12 +1,21 @@
 import React from "react";
+import { useRate } from "../../../../../../../../../hooks/useRate";
 import { ProductCardLink } from "./utils/ProductCard";
 
 function ProductLinks() {
+  const { products, loading } = useRate();
+
   return (
     <>
-      <div className="flex gap-6 flex-col">
-        <ProductCardLink />
-      </div>
+      {loading || products.length < 1 ? (
+        <></>
+      ) : (
+        <div className="flex gap-6 flex-col">
+          {products.map((e, i) => (
+            <ProductCardLink key={i} data={e} />
+          ))}
+        </div>
+      )}
     </>
   );
 }
--- src/screens/App/screens/User/screens/Orders/screens/Rate/components/utils/ProductCard.jsx
+++ src/screens/App/screens/User/screens/Orders/screens/Rate/components/utils/ProductCard.jsx
@@ -2,30 +2,32 @@ import React from "react";
 import { formatPrice } from "../../../../../../../../../../shared/utils/price";
 import { Link } from "react-router-dom";
 
-function ProductCardLink() {
+function ProductCardLink({
+  data: { imageAddress, item, retailPrice: price, orderId, productId },
+}) {
   return (
     <div className="shadow rounded-md flex flex-row">
       <img
-        src="https://asset1.ee.co.uk/medias/samsung-galaxy-z-fold3-5g-256gb-black-desktop-detail-1-Format-976?context=bWFzdGVyfHJvb3R8OTE4NjE0fGltYWdlL3BuZ3xzeXMtbWFzdGVyL3Jvb3QvaDI0L2hlZS85NzAzMjk0NDY4MTI2L3NhbXN1bmctZ2FsYXh5LXotZm9sZDMtNWctMjU2Z2ItYmxhY2stZGVza3RvcC1kZXRhaWwtMV9Gb3JtYXQtOTc2fDNmZDkyNjAyYWNjNmY4ZjcwNjA1MTVmMTcwZTIxMDUyOWNjZmZhYWMyMjc1ZDcwNmI4ZmE1M2FmMmZjYjFjYzk"
-        alt="product-image"
+        src={imageAddress}
+        alt="product"
         className="object-contain w-24 flex-shrink-0"
       />
 
       <div className="flex flex-row justify-between flex-grow flex-shrink px-4 py-2">
         <div className="flex flex-col justify-between">
-          <p className="text-xl font-medium text-gray-600">Smartphone</p>
-          <p className="text-my-accent text-lg font-medium">{`₱${formatPrice(
-            69999
-          )}`}</p>
+          <p className="text-xl font-medium text-gray-600">{item}</p>
+          <p className="text-my-accent text-lg font-medium">
+            {`₱${formatPrice(price)}`}
+          </p>
         </div>
 
         <div className="space-y-0 text-right">
           <p className="text-gray-400">Order ID</p>
           <Link
-            to="/users/orders?p=dsasdfasfas"
+            to={`/users/orders?oid=${orderId}`}
             className="transition duration-200 font-medium text-md text-gray-400 hover:text-gray-900"
           >
-            xsdf51231234szxcv8g213
+            {orderId}
           </Link>
         </div>
       </div>
