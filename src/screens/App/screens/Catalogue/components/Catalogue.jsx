2576e0657cf389f2f1b2ae5c0ea663cca858cf1e
gn-grc <genejogarcia.gg@gmail.com>
2021-10-19 23:42:35 +0800

    used containeer in catalogue page

--- src/screens/App/screens/Catalogue/components/Catalogue.jsx
+++ src/screens/App/screens/Catalogue/components/Catalogue.jsx
@@ -6,6 +6,7 @@ import Loading from "../../../../../shared/Loading/Loading";
 import useAlert from "../../../../../hooks/useAlert";
 import FilterButton from "./FilterButton";
 import useProductPagination from "../../../../../hooks/useProductPagination";
+import Container from "../../../../../shared/Components/pages/Container";
 
 function Catalogue() {
   const [loading, setLoading] = useState(true);
@@ -42,59 +43,63 @@ function Catalogue() {
     <>
       <Title title="Product Catalogue" />
 
-      <div className="flex flex-wrap sm:flex-nowrap my-12 mx-3 lg:mx-10 xl:mx-16 2xl:mx-28 gap-y-5 sm:gap-y-0">
-        <div className="w-full sm:w-52 flex flex-wrap flex-row sm:flex-col gap-y-10 justify-center sm:justify-start">
-          <div className="flex flex-col gap-y-1">
-            <span className="text-sm font-semibold text-gray-400">
-              Product View
-            </span>
-            <FilterButton name="Grid View" />
-            <FilterButton name="Board View" />
+      <Container>
+        <div className="flex flex-wrap sm:flex-nowrap my-12 mx-3 lg:mx-10 xl:mx-16 2xl:mx-28 gap-y-5 sm:gap-y-0">
+          <div className="w-full sm:w-52 flex flex-wrap flex-row sm:flex-col gap-y-10 justify-center sm:justify-start">
+            <div className="flex flex-col gap-y-1">
+              <span className="text-sm font-semibold text-gray-400">
+                Product View
+              </span>
+              <FilterButton name="Grid View" />
+              <FilterButton name="Board View" />
+            </div>
+
+            <div className="flex flex-col gap-y-1">
+              <span className="text-sm font-semibold text-gray-400">
+                Sort By
+              </span>
+              <FilterButton name="All-time Sales" />
+              <FilterButton name="Product Rating" />
+            </div>
+
+            <div className="flex flex-col gap-y-1">
+              <span className="text-sm font-semibold text-gray-400">
+                Order by
+              </span>
+              <FilterButton name="Highest Price" />
+              <FilterButton name="Lowest Price" />
+            </div>
+
+            <div className="flex flex-col gap-y-1">
+              <span className="text-sm font-semibold text-gray-400">
+                Price Range
+              </span>
+              <FilterButton name="Coming Soon..." />
+            </div>
+
+            <div className="flex flex-col gap-y-1">
+              <span className="text-sm font-semibold text-gray-400">
+                Product Category
+              </span>
+              <FilterButton name="Coming Soon..." />
+            </div>
           </div>
 
-          <div className="flex flex-col gap-y-1">
-            <span className="text-sm font-semibold text-gray-400">Sort By</span>
-            <FilterButton name="All-time Sales" />
-            <FilterButton name="Product Rating" />
-          </div>
-
-          <div className="flex flex-col gap-y-1">
-            <span className="text-sm font-semibold text-gray-400">
-              Order by
-            </span>
-            <FilterButton name="Highest Price" />
-            <FilterButton name="Lowest Price" />
-          </div>
-
-          <div className="flex flex-col gap-y-1">
-            <span className="text-sm font-semibold text-gray-400">
-              Price Range
-            </span>
-            <FilterButton name="Coming Soon..." />
-          </div>
-
-          <div className="flex flex-col gap-y-1">
-            <span className="text-sm font-semibold text-gray-400">
-              Product Category
-            </span>
-            <FilterButton name="Coming Soon..." />
+          <div className="flex-grow">
+            {loading ? (
+              <div>
+                <Loading />
+              </div>
+            ) : (
+              <div className="flex flex-col items-center  sm:grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-4 gap-x-8 gap-y-14">
+                {items.map((item) => (
+                  <ProductSmall data={item} key={item._id} />
+                ))}
+              </div>
+            )}
           </div>
         </div>
-
-        <div className="flex-grow">
-          {loading ? (
-            <div>
-              <Loading />
-            </div>
-          ) : (
-            <div className="flex flex-col items-center  sm:grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-4 gap-x-8 gap-y-14">
-              {items.map((item) => (
-                <ProductSmall data={item} key={item._id} />
-              ))}
-            </div>
-          )}
-        </div>
-      </div>
+      </Container>
     </>
   );
 }
