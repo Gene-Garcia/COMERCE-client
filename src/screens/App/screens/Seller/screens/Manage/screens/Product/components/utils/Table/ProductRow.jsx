9a1a48f2f48a516e1e3e7f387802547d12880df8
gn-grc <genejogarcia.gg@gmail.com>
2021-12-11 23:31:55 +0800

    added style of product heading and row

---
+++ src/screens/App/screens/Seller/screens/Manage/screens/Product/components/utils/Table/ProductRow.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+function ProductRow({ data }) {
+  const theme = "font-medium text-gray-400 text-base";
+
+  return (
+    <tr className="px-20 bg-white flex flex-row justify-between">
+      <td className="w-five flex justify-center">
+        <button className="h-4 w-4 bg-gray-400"></button>
+      </td>
+
+      <td className={`w-fifteen flex justify-center ${theme}`}>
+        Product Image
+      </td>
+
+      <td className={`w-ten flex justify-center ${theme}`}>ID</td>
+
+      <td className={`w-1/4 flex justify-center ${theme}`}>Product Name</td>
+
+      <td className={`w-fifteen flex justify-center ${theme}`}>Price</td>
+
+      <td className={`w-ten flex justify-center ${theme}`}>Total Stock</td>
+
+      <td className={`w-1/5 flex justify-center ${theme}`}>Actions</td>
+    </tr>
+  );
+}
+export default ProductRow;
