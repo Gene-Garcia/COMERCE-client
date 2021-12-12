438dcb51719d89e120bc8aa8f54752e18a558d04
gn-grc <genejogarcia.gg@gmail.com>
2021-12-12 22:51:35 +0800

    updated product form inputs for useForm preperation

--- src/screens/App/screens/Seller/screens/Manage/screens/Product/components/utils/ProductForm.jsx
+++ src/screens/App/screens/Seller/screens/Manage/screens/Product/components/utils/ProductForm.jsx
@@ -1,51 +1,85 @@
 import React from "react";
 import Button from "../../../../../../../../../../shared/Components/button/Button";
 
-function NameInput() {
+function NameInput({ name, value, error, onChange, placeholder }) {
   return (
     <div className="flex flex-col">
       <input
         type="text"
-        name=""
-        className="w-full focus:outline-none text-lg bg-transparent border-b-2 border-gray-300 transition duration-200 ease-linear focus:border-my-accent"
+        name={name}
+        value={value}
+        onChange={onChange}
+        placeholder={placeholder}
+        className={`px-2 py-1.5 w-full text-lg bg-transparent shadow rounded 
+        border-b-2 
+        ${error ? "border-red-500" : "border-gray-300"}
+        transition duration-200 ease-linear 
+        focus:border-my-accent focus:outline-none`}
       />
-      <label for="" className="uppercase font-semibold text-gray-400 text-sm">
-        Product Name
-      </label>
+
+      <div className="flex flex-row justify-between">
+        <label for="" className="uppercase font-semibold text-gray-400 text-sm">
+          Product Name
+        </label>
+
+        <span className="text-red-500 font-regular">{error}</span>
+      </div>
     </div>
   );
 }
 
-function PriceInput() {
+function PriceInput({ name, value, error, onChange, placeholder }) {
   return (
-    <div className="flex flex-col">
-      <label for="" className="uppercase font-semibold text-gray-400 text-sm">
-        PRICE
-      </label>
+    <div className="flex flex-col w-2/5">
+      <div className="flex flex-row justify-between">
+        <label for="" className="uppercase font-semibold text-gray-400 text-sm">
+          PRICE
+        </label>
+
+        <span className="text-red-500 font-regular">{error}</span>
+      </div>
+
       <div className="inline-flex gap-2">
-        <p className="rounded shadow bg-my-white-tint text-my-accent font-medium text-md px-2 py-1.5 border border-transparent">
+        <p className="rounded shadow bg-my-white-tint text-my-accent font-semibold text-md px-2 py-1.5 border border-transparent">
           PHP
         </p>
         <input
           type="number"
-          className="rounded bg-my-white-tint rounded w-1/4 px-2 py-1.5 shadow focus:outline-none border border-transparent transition ease-linear focus:border-my-accent"
-          value="12"
+          value={value}
+          name={name}
+          onChange={onChange}
+          placeholder={placeholder}
+          className={`rounded bg-my-white-tint rounded w-full px-2 py-1.5 shadow 
+          border ${error ? "border-red-500" : "border-transparent"}
+          transition ease-linear 
+          focus:border-my-accent focus:outline-none `}
         />
       </div>
     </div>
   );
 }
 
-function DataListInput() {
+function DataListInput({ name, value, error, onChange }) {
   return (
-    <div className="flex flex-col">
-      <label className="uppercase font-semibold text-gray-400 text-sm">
-        CATEGORY
-      </label>
+    <div className="flex flex-col w-1/2">
+      <div className="flex flex-row justify-between">
+        <label className="uppercase font-semibold text-gray-400 text-sm">
+          CATEGORY
+        </label>
+
+        <span className="text-red-500 font-regular">{error}</span>
+      </div>
+
       <input
         type="text"
+        name={name}
+        value={value}
+        onChange={onChange}
         list="category-list"
-        className="focus:outline-none rounded bg-my-white-tint px-2 py-1.5 text-base w-1/2 shadow border border-transparent transition ease-linear focus:border-my-accent"
+        className={`rounded bg-my-white-tint px-2 py-1.5 text-base shadow 
+        border ${error ? "border-red-500" : "border-transparent"}
+        transition ease-linear 
+        focus:border-my-accent focus:outline-none`}
       />
 
       <datalist id="category-list">
@@ -55,7 +89,7 @@ function DataListInput() {
   );
 }
 
-function GalleryInput() {
+function GalleryInput({ name, value, error, onChange }) {
   return (
     <div className="space-y-2">
       {/* preview of image */}
@@ -65,59 +99,92 @@ function GalleryInput() {
         </span>
       </div>
 
-      <label class="h-16 w-16 flex items-center justify-center cursor-pointer text-gray-800 bg-my-white-tint transition duration-200 ease-linear hover:bg-gray-200 hover:shadow active:text-my-accent">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="h-8 w-8"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth={2}
-            d="M12 4v16m8-8H4"
+      <div className="flex flex-row gap-4">
+        <label class="h-16 w-16 flex items-center justify-center cursor-pointer text-gray-800 bg-my-white-tint transition duration-200 ease-linear hover:bg-gray-200 hover:shadow active:text-my-accent">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-8 w-8"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M12 4v16m8-8H4"
+            />
+          </svg>
+
+          <input
+            type="file"
+            className="hidden"
+            accept="image"
+            // name={name}
+            // onChange={onChange}
+            // value={value}
           />
-        </svg>
-        <input
-          type="file"
-          className="hidden"
-          accept="image"
-          // name={name}
-          // onChange={onChange}
+        </label>
+
+        <DefaultInput
+          label="Temporary File Input"
+          name={name}
+          value={value}
+          error={error}
+          onChange={onChange}
+          width="w-full"
         />
-      </label>
+      </div>
     </div>
   );
 }
 
-function DefaultInput({ label, helper, width }) {
+function DefaultInput({ label, helper, width, name, value, error, onChange }) {
   return (
-    <div className="flex flex-col">
-      <label for="" className="uppercase font-semibold text-gray-400 text-sm">
-        {label}
-      </label>
+    <div className={`flex flex-col ${width}`}>
+      <div className="flex flex-row justify-between">
+        <label for="" className="uppercase font-semibold text-gray-400 text-sm">
+          {label}
+        </label>
+
+        <span className="text-red-500 font-regular">{error}</span>
+      </div>
+
       <input
         type="text"
-        name=""
-        value="asdfasdf"
-        className={`${width} px-2 py-1.5 text-base focus:outline-none shadow rounded border border-transparent transition ease-linear focus:border-my-accent`}
+        name={name}
+        value={value}
+        onChange={onChange}
+        className={`px-2 py-1.5 text-base shadow rounded 
+        border ${error ? "border-red-500" : "border-transparent"}
+        transition ease-linear 
+        focus:outline-none focus:border-my-accent`}
       />
       <i className="text-sm text-gray-500">{helper}</i>
     </div>
   );
 }
 
-function AreaInput() {
+function AreaInput({ label, name, value, error, onChange }) {
   return (
     <div className="flex flex-col">
-      <label className="uppercase font-semibold text-gray-400 text-sm">
-        CATEGORY
-      </label>
+      <div className="flex flex-row justify-between">
+        <label className="uppercase font-semibold text-gray-400 text-sm">
+          {label}
+        </label>
+
+        <span className="text-red-500 font-regular">{error}</span>
+      </div>
+
       <textarea
         rows="3"
-        className="px-2 py-1.5 text-base focus:outline-none rounded shadow border border-transparent transition ease-linear focus:border-my-accent"
+        name={name}
+        value={value}
+        onChange={onChange}
+        className={`px-2 py-1.5 text-base rounded shadow 
+        border ${error ? "border-red-500" : "border-transparent"}
+        transition ease-linear 
+        focus:outline-none focus:border-my-accent`}
       ></textarea>
     </div>
   );
