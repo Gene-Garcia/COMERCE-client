250c30329ce4677dc1259fd89e46ae7816b27eba
gn-grc <genejogarcia.gg@gmail.com>
2021-12-19 13:41:22 +0800

    added sign out seller navigation link to settings group

--- src/screens/App/components/sellerNavigation/Navigation.jsx
+++ src/screens/App/components/sellerNavigation/Navigation.jsx
@@ -34,14 +34,6 @@ function Sidebar() {
           {Object.entries(sellerNavLinks).map(([k, v]) => (
             <SidebarGroup key={k} groupName={v.name} links={v.links} />
           ))}
-
-          <div className="py-4">
-            <SidebarLink
-              svgD="M11 16l-4-4m0 0l4-4m-4 4h14m-5 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h7a3 3 0 013 3v1"
-              name="LOG OUT"
-              to="#"
-            />
-          </div>
         </div>
       </div>
     </div>
