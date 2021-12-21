6c83d5b7e416664ba74c37210486688b698215d3
gn-grc <genejogarcia.gg@gmail.com>
2021-12-21 14:32:00 +0800

    integrated use form in add inventory

--- src/screens/App/screens/Seller/screens/Manage/screens/Inventory/components/SelectedProduct/AddInventoryForm.jsx
+++ src/screens/App/screens/Seller/screens/Manage/screens/Inventory/components/SelectedProduct/AddInventoryForm.jsx
@@ -1,15 +1,87 @@
 import React from "react";
+import { useForm } from "../../../../../../../../../../hooks/useForm";
 import Button from "../../../../../../../../../../shared/Components/button/Button";
 import { DefaultInput } from "../../../../../../../../../../shared/Components/seller/InputField";
 
 function AddInventoryForm() {
+  // makes the value of the onHand same as the inventory
+  const copyQuantity = () => {
+    if (values.inventory != "" && !isNaN(values.inventory)) {
+      setValues((prev) => ({ ...prev, ["onHand"]: prev.inventory }));
+    }
+  };
+
+  const SubmitNewInventory = () => {};
+
+  const validate = (data, setErrors) => {
+    let tempErrs = { ...errors };
+
+    if ("inventory" in data) {
+      tempErrs.inventory = data.inventory ? "" : "Required";
+
+      if (!tempErrs.inventory)
+        tempErrs.inventory = isNaN(data.inventory) ? "Must be numeric" : "";
+
+      if (!tempErrs.inventory) {
+        const temp = parseInt(data.inventory);
+        tempErrs.inventory = temp < 0 ? "No negative value" : "";
+      }
+    }
+
+    if ("onHand" in data) {
+      tempErrs.onHand = data.onHand ? "" : "Required";
+
+      if (!tempErrs.onHand)
+        tempErrs.onHand = isNaN(data.onHand) ? "Must be numeric" : "";
+
+      if (!tempErrs.onHand) {
+        const temp = parseInt(data.onHand);
+        tempErrs.onHand = temp < 0 ? "No negative value" : "";
+      }
+
+      if (!tempErrs.onHand) {
+        const tempOnHand = parseInt(data.onHand);
+        const tempInventory = parseInt(values.inventory);
+        tempErrs.onHand =
+          tempOnHand > tempInventory ? "Higher than inventory" : "";
+      }
+    }
+
+    setErrors(tempErrs);
+  };
+
+  const init = {
+    onHand: "",
+    inventory: "",
+  };
+  const {
+    values,
+    errors,
+    isLoading,
+    setIsLoading,
+    resetForms,
+    handleInput,
+    handleFormSubmit,
+    setValues,
+  } = useForm(init, init, validate, SubmitNewInventory);
+
   return (
     <div className="flex flex-col gap-y-5">
       <p className="font-semibold text-gray-600 text-md">Add Inventory</p>
 
       <div className="inline-flex items-center gap-8">
-        <DefaultInput type="number" label="Inventory" width="w-auto" />{" "}
+        <DefaultInput
+          type="number"
+          label="Inventory"
+          width="w-auto"
+          name="inventory"
+          value={values.inventory}
+          error={errors.inventory}
+          onChange={handleInput}
+        />
+
         <button
+          onClick={copyQuantity}
           className="text-gray-700 rounded p-2 
         place-self-end
         transition duration-150 ease-linear
@@ -30,11 +102,21 @@ function AddInventoryForm() {
             />
           </svg>
         </button>
-        <DefaultInput type="number" label="Onhand" width="w-auto" />
+
+        <DefaultInput
+          type="number"
+          label="Onhand"
+          width="w-auto"
+          name="onHand"
+          value={values.onHand}
+          error={errors.onHand}
+          onChange={handleInput}
+        />
       </div>
 
       <Button
-        isLoading={false}
+        isLoading={isLoading}
+        onClick={handleFormSubmit}
         buttonClass="uppercase text-sm font-semibold text-white
       bg-my-accent py-2.5 px-7 w-max rounded
       transition duration-200 ease-linear
