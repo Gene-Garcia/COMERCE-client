e815fee17a103b05a39f15566575613aea8a73b5
gn-grc <genejogarcia.gg@gmail.com>
2022-03-01 11:40:23 +0800

    added ship orders reducer

---
+++ src/redux/Seller/ShipOrders/ShipOrdersReducer.js
@@ -0,0 +1,48 @@
+import { shopOrdersActionTypes as types } from "./ShopOrdersAction";
+
+const initial = {
+  pendingOrders: [],
+
+  isModalOpen: false,
+  modalOrder: null,
+};
+
+const shipOrdersReducer = (state = initial, { type, payload }) => {
+  switch (type) {
+    case types.LOAD_PENDING_ORDERS:
+      return { ...state, pendingOrders: payload };
+
+    case types.UPDATE_MODALED_ORDER:
+      return { ...state, modalOrder: payload };
+
+    case types.TRIGGER_MODAL_STATE:
+      return { ...state, isModalOpen: payload };
+
+    case types.CHECK_THIS_ORDER:
+      return {
+        ...state,
+        pendingOrders: state.pendingOrders.map((order) =>
+          order._id === payload.orderId
+            ? { ...order, checked: payload.status }
+            : { ...order }
+        ),
+      };
+
+    case types.CHECK_ALL_ORDERS:
+      return {
+        ...state,
+        pendingOrders: state.pendingOrders.map((order) => ({
+          ...order,
+          checked: payload,
+        })),
+      };
+
+    case types.RESET_TO_DEFAULT:
+      return { ...initial };
+
+    default:
+      return { ...state };
+  }
+};
+
+export default shipOrdersReducer;
--- src/redux/Seller/ShipOrders/ShopOrdersAction.js
+++ src/redux/Seller/ShipOrders/ShopOrdersAction.js
@@ -1,5 +1,59 @@
-// load pending orders
+const shopOrdersActionTypes = {
+  LOAD_PENDING_ORDERS: "LOAD_PENDING_ORDERS",
+  UPDATE_MODALED_ORDER: "UPDATE_MODALED_ORDER",
+  TRIGGER_MODAL_STATE: "TRIGGER_MODAL_STATE",
+  CHECK_THIS_ORDER: "CHECK_THIS_ORDER",
+  CHECK_ALL_ORDERS: "CHECK_ALL_ORDERS",
+  RESET_TO_DEFAULT: "RESET_TO_DEFAULT",
+};
 
-// information modal
+const loadPendingOrders = (pendingOrders) => {
+  return {
+    type: shopOrdersActionTypes.LOAD_PENDING_ORDERS,
+    payload: pendingOrders,
+  };
+};
 
-// set all item to checked
+const updateModaledOrder = (order) => {
+  return {
+    type: shopOrdersActionTypes.UPDATE_MODALED_ORDER,
+    payload: order,
+  };
+};
+
+const triggerModalState = (isOpen) => {
+  return {
+    type: shopOrdersActionTypes.TRIGGER_MODAL_STATE,
+    payload: isOpen,
+  };
+};
+
+const checkThisOrder = (orderId, status) => {
+  return {
+    type: shopOrdersActionTypes.CHECK_THIS_ORDER,
+    payload: { orderId, status },
+  };
+};
+
+const checkAllOrders = (status) => {
+  return {
+    type: shopOrdersActionTypes.CHECK_ALL_ORDERS,
+    payload: status,
+  };
+};
+
+const resetToDefault = () => {
+  return {
+    type: shopOrdersActionTypes.RESET_TO_DEFAULT,
+  };
+};
+
+export {
+  shopOrdersActionTypes,
+  loadPendingOrders,
+  updateModaledOrder,
+  triggerModalState,
+  checkThisOrder,
+  checkAllOrders,
+  resetToDefault,
+};
