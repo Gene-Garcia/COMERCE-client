667710ffafa1bdd9796d39054bf24a2c6a6b65f9
gn-grc <genejogarcia.gg@gmail.com>
2022-03-10 23:22:36 +0800

    corrected name of rate order redux

---
+++ src/redux/RateOrder/RateOrderAction.js
@@ -0,0 +1,76 @@
+const rateOrderActionTypes = {
+  LOAD_PRODUCTS: "LOAD_PRODUCTS",
+  UPDATE_SELECTED_PRODUCT: "UPDATE_SELECTED_PRODUCT",
+  UPDATE_PRODUCT_RATING_STATUS: "UPDATE_PRODUCT_RATING_STATUS",
+  TOGGLE_NEXT_PRODUCT_TO_RATE: "TOGGLE_NEXT_PRODUCT_TO_RATE",
+  SET_RATING: "SET_RATING",
+  ON_COMMENT_CHANGE: "ON_COMMENT_CHANGE",
+  UPDATE_LOADING: "UPDATE_LOADING",
+  RESET_VALUES: "RESET_VALUES",
+};
+
+const loadProducts = (products) => {
+  return {
+    type: rateOrderActionTypes.LOAD_PRODUCTS,
+    payload: products,
+  };
+};
+
+const updateSelectedProduct = (product) => {
+  return {
+    type: rateOrderActionTypes.UPDATE_SELECTED_PRODUCT,
+    payload: product,
+  };
+};
+
+const updateProductRatingStatus = (productId, orderId, rateStatus) => {
+  return {
+    type: rateOrderActionTypes.UPDATE_PRODUCT_RATING_STATUS,
+    payload: { productId, orderId, rateStatus },
+  };
+};
+
+const toggleNextProductToRate = () => {
+  return {
+    type: rateOrderActionTypes.TOGGLE_NEXT_PRODUCT_TO_RATE,
+  };
+};
+
+const setRating = (rate) => {
+  return {
+    type: rateOrderActionTypes.SET_RATING,
+    payload: rate,
+  };
+};
+
+const onCommentChange = (event) => {
+  return {
+    type: rateOrderActionTypes.ON_COMMENT_CHANGE,
+    payload: event.target.value,
+  };
+};
+
+const updateLoading = (isLoading) => {
+  return {
+    type: rateOrderActionTypes.UPDATE_LOADING,
+    payload: isLoading,
+  };
+};
+
+const resetValues = () => {
+  return {
+    type: rateOrderActionTypes.RESET_VALUES,
+  };
+};
+
+export {
+  rateOrderActionTypes,
+  loadProducts,
+  updateSelectedProduct,
+  updateProductRatingStatus,
+  toggleNextProductToRate,
+  setRating,
+  onCommentChange,
+  updateLoading,
+  resetValues,
+};
---
+++ src/redux/RateOrder/RateOrderReducer.js
@@ -0,0 +1,60 @@
+import { rateOrderActionTypes as types } from "./RateOrderAction";
+
+const initial = {
+  products: [],
+  selectedProduct: null,
+  /*  */
+  rating: -1,
+  comment: "",
+  loading: true,
+};
+
+const rateOrderReducer = (state = initial, { type, payload }) => {
+  switch (type) {
+    case types.LOAD_PRODUCTS:
+      return { ...state, products: payload };
+
+    case types.UPDATE_SELECTED_PRODUCT:
+      return { ...state, selectedProduct: payload };
+
+    /*
+     * iterates and finds in the product the pId and oId, and
+     * updates its rated to new rateStatus
+     */
+    case types.UPDATE_PRODUCT_RATING_STATUS:
+      return {
+        ...state,
+        products: state.products.map((e) =>
+          e.productId === payload.productId && e.orderId === payload.orderId
+            ? { ...e, rated: payload.rateStatus }
+            : e
+        ),
+      };
+
+    // finds any product that has rated false
+    case types.TOGGLE_NEXT_PRODUCT_TO_RATE:
+      return {
+        ...state,
+        selectedProduct: state.products.find((e) => e.rated === false),
+        rating: -1,
+        comment: "",
+      };
+
+    case types.SET_RATING:
+      return { ...state, rating: payload };
+
+    case types.ON_COMMENT_CHANGE:
+      return { ...state, comment: payload };
+
+    case types.UPDATE_LOADING:
+      return { ...state, loading: payload };
+
+    case types.RESET_VALUES:
+      return { ...initial };
+
+    default:
+      return { ...state };
+  }
+};
+
+export default rateOrderReducer;
