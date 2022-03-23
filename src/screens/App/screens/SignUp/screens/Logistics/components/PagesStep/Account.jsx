c1b9f4e4b55d2ca56606d19702ec3ece6695d366
gn-grc <genejogarcia.gg@gmail.com>
2022-03-23 15:46:11 +0800

    removed the personal details form-related in the account form

--- src/screens/App/screens/SignUp/screens/Logistics/components/PagesStep/Account.jsx
+++ src/screens/App/screens/SignUp/screens/Logistics/components/PagesStep/Account.jsx
@@ -10,32 +10,6 @@ const Account = () => {
         <div className="flex flex-row justify-between gap-5">
           <EmbossedInput
             type="text"
-            name="fName"
-            value=""
-            onChange={() => {}}
-            placeholder="Your first name"
-            background="bg-gray-100"
-            label="First Name"
-            error=""
-            width="w-full"
-          />
-
-          <EmbossedInput
-            type="text"
-            name="lName"
-            value=""
-            onChange={() => {}}
-            placeholder="Your last name"
-            background="bg-gray-100"
-            label="Last Name"
-            error=""
-            width="w-full"
-          />
-        </div>
-
-        <div className="flex flex-row justify-between gap-5">
-          <EmbossedInput
-            type="text"
             name="username"
             value=""
             onChange={() => {}}
@@ -70,95 +44,9 @@ const Account = () => {
           error=""
           width="w-full"
         />
-
-        <EmbossedInput
-          type="text"
-          name="streetAddress"
-          value=""
-          onChange={() => {}}
-          placeholder="Your street level address"
-          background="bg-gray-100"
-          label="Street Address"
-          error=""
-          width="w-full"
-        />
-
-        <div className="flex flex-row justify-between gap-5">
-          <EmbossedInput
-            type="text"
-            name="barangay"
-            value=""
-            onChange={() => {}}
-            placeholder="The name of your barangay"
-            background="bg-gray-100"
-            label="Barangay"
-            error=""
-            width="w-full"
-          />
-
-          <EmbossedInput
-            type="text"
-            name="cityMunicipality"
-            value=""
-            onChange={() => {}}
-            placeholder="Name of your city or municipality"
-            background="bg-gray-100"
-            label="City/Municipality"
-            error=""
-            width="w-full"
-          />
-
-          <EmbossedInput
-            type="text"
-            name="province"
-            value=""
-            onChange={() => {}}
-            placeholder="Name of your province"
-            background="bg-gray-100"
-            label="Province"
-            error=""
-            width="w-full"
-          />
-        </div>
-
-        <div className="flex flex-row justify-between gap-5">
-          <EmbossedInput
-            type="number"
-            name="primaryNumber"
-            value=""
-            onChange={() => {}}
-            placeholder="Your primary contact number"
-            background="bg-gray-100"
-            label="Primary Contact Number"
-            error=""
-            width="w-full"
-            icon={
-              <span className="mx-2 font-medium text-my-accent text-base">
-                +63
-              </span>
-            }
-          />
-
-          <EmbossedInput
-            type="number"
-            name="secondaryNumber"
-            value=""
-            onChange={() => {}}
-            placeholder="Secondary contact number"
-            background="bg-gray-100"
-            label="Secondary Contact Number"
-            error=""
-            width="w-full"
-            icon={
-              <span className="mx-2 font-medium text-my-accent text-base">
-                +63
-              </span>
-            }
-          />
-        </div>
       </div>
 
-      <AccountCTA />
+      <AccountCTA onClick={() => {}} />
     </>
   );
 };
