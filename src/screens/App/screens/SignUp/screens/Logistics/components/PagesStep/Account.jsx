08c4e1ff194e4a8d094e767014e14f5d92102ef7
gn-grc <genejogarcia.gg@gmail.com>
2022-03-24 13:52:08 +0800

    implemented user form in account

--- src/screens/App/screens/SignUp/screens/Logistics/components/PagesStep/Account.jsx
+++ src/screens/App/screens/SignUp/screens/Logistics/components/PagesStep/Account.jsx
@@ -1,8 +1,42 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+import { useForm } from "../../../../../../../../hooks/useForm";
+import { loadAccountData } from "../../../../../../../../redux/Logistics/LogisticsRegistration/LogisticsRegistrationAction";
 import { EmbossedInput } from "../../../../../../../../shared/Components/input/Inputs";
 import { AccountCTA } from "./CallToAction";
 
 const Account = () => {
+  // redux
+  const dispatch = useDispatch();
+
+  const SubmitLogisticsRegistration = async () => {
+    dispatch(loadAccountData(values));
+  };
+
+  const validate = (data, setErrors) => {
+    const temp = { ...errors };
+
+    if ("username" in data) temp.username = data.username ? "" : "Required";
+    if ("email" in data) temp.email = data.email ? "" : "Required";
+    if ("password" in data) temp.password = data.password ? "" : "Required";
+
+    setErrors(temp);
+  };
+
+  const initial = {
+    username: "",
+    email: "",
+    password: "",
+  };
+  const {
+    values,
+    errors,
+    handleInput,
+    handleFormSubmit,
+    setIsLoading,
+    isLoading,
+  } = useForm(initial, initial, validate, SubmitLogisticsRegistration);
+
   return (
     <>
       {/* form */}
@@ -11,24 +45,24 @@ const Account = () => {
           <EmbossedInput
             type="text"
             name="username"
-            value=""
-            onChange={() => {}}
+            value={values.username}
+            onChange={handleInput}
             placeholder="How would you like to be addressed"
             background="bg-gray-100"
             label="Username"
-            error=""
+            error={errors.username}
             width="w-full"
           />
 
           <EmbossedInput
             type="email"
             name="email"
-            value=""
-            onChange={() => {}}
+            value={values.email}
+            onChange={handleInput}
             placeholder="Enter your unique email"
             background="bg-gray-100"
             label="Email"
-            error=""
+            error={errors.email}
             width="w-full"
           />
         </div>
@@ -36,17 +70,17 @@ const Account = () => {
         <EmbossedInput
           type="password"
           name="password"
-          value=""
-          onChange={() => {}}
+          value={values.password}
+          onChange={handleInput}
           placeholder="Strong password for your account"
           background="bg-gray-100"
           label="Account Password"
-          error=""
+          error={errors.password}
           width="w-full"
         />
       </div>
 
-      <AccountCTA onClick={() => {}} />
+      <AccountCTA onClick={handleFormSubmit} isLoading={isLoading} />
     </>
   );
 };
