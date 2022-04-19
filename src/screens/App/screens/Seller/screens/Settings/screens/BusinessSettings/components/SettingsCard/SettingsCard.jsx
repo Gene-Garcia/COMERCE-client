5b0d51201c77bbaa946a27010c4323d0e521c00f
gn-grc <genejogarcia.gg@gmail.com>
2022-04-19 22:57:07 +0800

    added base card container for settings

---
+++ src/screens/App/screens/Seller/screens/Settings/screens/BusinessSettings/components/SettingsCard/SettingsCard.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const SettingsCard = ({ className, title, children }) => {
+  return (
+    <div className={`bg-my-white-tint shadow-md rounded-md p-8 ${className}`}>
+      <h3 className="font-medium text-gray-700">{title}</h3>
+
+      <div>{children}</div>
+    </div>
+  );
+};
+export default SettingsCard;
