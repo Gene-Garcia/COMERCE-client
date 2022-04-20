4b7298d8227be0445d9bf1864de90dbcf1348de6
gn-grc <genejogarcia.gg@gmail.com>
2022-04-20 23:01:28 +0800

    implemented consuming of other business information API route

--- src/screens/App/screens/Seller/screens/Settings/screens/BusinessSettings/components/SettingsCard/CardContents/OtherInformationCard.jsx
+++ src/screens/App/screens/Seller/screens/Settings/screens/BusinessSettings/components/SettingsCard/CardContents/OtherInformationCard.jsx
@@ -1,23 +1,89 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "../../../../../../../../../../../shared/caller";
+import { batch, useDispatch } from "react-redux";
+import { useHistory } from "react-router-dom";
+import {
+  setMessage,
+  setSeverity,
+} from "../../../../../../../../../../../redux/Alert/AlertAction";
+import { formatDate } from "../../../../../../../../../../../shared/utils/date";
 
 const OtherInformationCard = () => {
+  const history = useHistory();
+
+  // redux
+  const dispatch = useDispatch();
+
+  // data
+  const [loading, setLoading] = useState(true);
+  const [email, setEmail] = useState("");
+  const [established, setEstablished] = useState("");
+  const [created, setCreated] = useState("");
+
+  // retrieve business information
+  useEffect(() => {
+    async function getOtherBusinessInformation() {
+      await axios
+        .get("/api/seller/business/other-information")
+        .then((res) => {
+          if (res.status === 200) {
+            console.log(res.data);
+            setEmail(res.data.businessEmail);
+            setEstablished(res.data.established);
+            setCreated(res.data.dateCreated);
+
+            setLoading(false);
+          }
+        })
+        .catch((err) => {
+          if (!err.response)
+            batch(() => {
+              dispatch(setSeverity("error"));
+              dispatch(
+                setMessage(
+                  "Something went wrong. Refresh the page or try again later."
+                )
+              );
+            });
+          else if (err.response.status === 403) history.push("/forbidden");
+          else if (err.response.status === 401) history.push("/unauthorized");
+          else {
+            batch(() => {
+              dispatch(setSeverity("error"));
+              dispatch(setMessage(err.response.data.error));
+            });
+          }
+        });
+    }
+
+    getOtherBusinessInformation();
+  }, []);
+
+  // clean up
+  useEffect(() => {
+    return () => {};
+  }, []);
   return (
     <div className="space-y-4">
-      <UneditableInput
-        width="w-3/4"
-        text="account@apple.com"
-        label="Business Email"
-      />
-      <UneditableInput
-        width="w-1/2"
-        text="November 15, 2021"
-        label="ESTABLISHED"
-      />
-      <UneditableInput
-        width="w-1/2"
-        text="April 20, 2022"
-        label="DATE CREATED"
-      />
+      {loading ? (
+        <>
+          <p>Loading</p>
+        </>
+      ) : (
+        <>
+          <UneditableInput width="w-3/4" text={email} label="Business Email" />
+          <UneditableInput
+            width="w-1/2"
+            text={formatDate(established, 1)}
+            label="ESTABLISHED"
+          />
+          <UneditableInput
+            width="w-1/2"
+            text={formatDate(created, 1)}
+            label="DATE CREATED"
+          />
+        </>
+      )}
     </div>
   );
 };
