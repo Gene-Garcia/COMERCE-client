c5dd8ad0057d622b879b646c7025de2ee5b2e1b2
gn-grc <genejogarcia.gg@gmail.com>
2022-06-15 12:39:35 +0800

    added api call to get for pack orders

--- src/screens/App/screens/Seller/screens/Shipment/screens/Pack/components/Pack.jsx
+++ src/screens/App/screens/Seller/screens/Shipment/screens/Pack/components/Pack.jsx
@@ -1,9 +1,59 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { SellerContainer } from "../../../../../../../../../shared/Components/pages/Container";
 import { SellerTitle } from "../../../../../../../../../shared/Components/pages/Title";
 import HeaderButton from "../../../../../../../../../shared/Components/seller/HeaderButton";
+import axios from "../../../../../../../../../shared/caller";
+import { useHistory } from "react-router-dom";
+import { batch, useDispatch } from "react-redux";
+import {
+  setMessage,
+  setSeverity,
+} from "../../../../../../../../../redux/Alert/AlertAction";
 
 const Pack = () => {
+  const history = useHistory();
+
+  // redux
+  const dispatch = useDispatch();
+
+  // loading var
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function getForPackOrders() {
+      await axios
+        .get("/api/seller/logistics/for/pack")
+        .then((res) => {
+          setLoading(false);
+          if (res.status === 200) {
+            console.log(res.data.orders);
+          }
+        })
+        .catch((err) => {
+          setLoading(false);
+
+          if (!err.response)
+            batch(() => {
+              dispatch(setSeverity("error"));
+              dispatch(
+                setMessage(
+                  "Something went wrong. Please refresh your browser and try again."
+                )
+              );
+            });
+          else if (err.response.status === 401) history.push("/unauthorized");
+          else if (err.response.status === 403) history.push("/forbidden");
+          else
+            batch(() => {
+              dispatch(setSeverity("error"));
+              dispatch(setMessage(err.response.data.error));
+            });
+        });
+    }
+
+    getForPackOrders();
+  }, []);
+
   return (
     <SellerContainer>
       <div className="flex flex-col xs:flex-row gap-4 xs:gap-0 items-center justify-between">
