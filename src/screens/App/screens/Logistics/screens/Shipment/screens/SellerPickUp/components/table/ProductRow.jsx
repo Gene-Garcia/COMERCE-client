78489aaca71076b9dbb9b9df912145285d251f9a
gn-grc <genejogarcia.gg@gmail.com>
2022-07-10 22:08:51 +0800

    added API request to PICK UP products

--- src/screens/App/screens/Logistics/screens/Shipment/screens/SellerPickUp/components/table/ProductRow.jsx
+++ src/screens/App/screens/Logistics/screens/Shipment/screens/SellerPickUp/components/table/ProductRow.jsx
@@ -1,4 +1,11 @@
 import React, { useState } from "react";
+import { batch, useDispatch } from "react-redux";
+import { useHistory } from "react-router-dom";
+import {
+  setMessage,
+  setSeverity,
+} from "../../../../../../../../../../redux/Alert/AlertAction";
+import axios from "../../../../../../../../../../shared/axios";
 import {
   Data,
   Row,
@@ -20,14 +27,60 @@ const ProductRow = ({
     orders,
     checked,
   },
+  product,
 }) => {
   const [collapsed, setCollapsed] = useState(false);
+
+  const history = useHistory();
+
+  // redux
+  const dispatch = useDispatch();
+
+  //#region API to PACK ORDER
+  const PackOrderAPI = async () => {
+    console.log("Requesting");
+    console.log(product);
+    await axios
+      .post("/api/logistics/orders/pick-up", {
+        products: { [businessId]: product },
+      })
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        if (!err.response)
+          batch(() => {
+            dispatch(setSeverity("error"));
+            dispatch(
+              setMessage(
+                "Something went wrong in retrieving your orders. Refresh your browser or try again later."
+              )
+            );
+          });
+        else if (err.response.status === 403) history.push("/forbidden");
+        else if (err.response.status === 401) history.push("unathorized");
+        else
+          batch(() => {
+            dispatch(setSeverity("error"));
+            dispatch(setMessage(err.response.data.error));
+          });
+      });
+  };
+  //#endregion
+
+  // checkbox change
+  const checkboxChange = (e) => {};
 
   return (
     <>
       <Row grid="grid-cols-14">
         <Data className="col-span-1 text-center">
-          <input type="checkbox" checked={checked} className="" />
+          <input
+            type="checkbox"
+            checked={checked}
+            className=""
+            onChange={checkboxChange}
+          />
         </Data>
         <Data className="col-span-1 font-light text-xs break-all">
           {businessId}
@@ -45,11 +98,9 @@ const ProductRow = ({
             <Action
               type="BUTTON"
               text="Show Parcels"
-              onClick={() => {
-                setCollapsed((prev) => !prev);
-              }}
+              onClick={() => setCollapsed((prev) => !prev)}
             />
-            <Action type="BUTTON" text="Pick Up" onClick={() => {}} />
+            <Action type="BUTTON" text="Pick Up" onClick={PackOrderAPI} />
           </ActionGroup>
         </Data>
       </Row>
