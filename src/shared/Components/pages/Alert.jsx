2dd185bec37d11f50ee83d0af2a9baf51ee381e0
gn-grc <genejogarcia.gg@gmail.com>
2022-07-11 19:41:42 +0800

    implemented stacked alert messages

--- src/shared/Components/pages/Alert.jsx
+++ src/shared/Components/pages/Alert.jsx
@@ -10,7 +10,7 @@
 
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { setMessage } from "../../../redux/Alert/AlertAction";
+import { filterMessages, setMessage } from "../../../redux/Alert/AlertAction";
 
 // change implementation
 function Alert() {
@@ -25,9 +25,14 @@ function Alert() {
   else if (severity === "information") color = "bg-blue-100 border-blue-500";
 
   // auto close of notication after certain seconds
-  setTimeout(() => {
-    dispatch(setMessage(""));
-  }, 3500);
+
+  useEffect(() => {
+    let interval = setTimeout(() => {
+      dispatch(setMessage(""));
+    }, 3500);
+
+    return () => clearInterval(interval);
+  }, [message, severity]);
 
   return (
     <div className={(message ? "fixed" : "hidden") + " top-32 left-1/2 z-10"}>
@@ -63,11 +68,133 @@ function Alert() {
 
 export default Alert;
 
-/*
+const StackedAlert = () => {
+  // redux alert states
+  const messages = useSelector((state) => state.ALERT.messages);
 
-so the new implementation will be also a single responsibility principle
+  return messages ? (
+    <div className={`fixed bottom-5 right-5 z-50 flex flex-col gap-3`}>
+      {Object.entries(messages).map(([id, v], i) => (
+        <AlertBody key={id} iteration={i + 1} id={id} {...v} />
+      ))}
+    </div>
+  ) : (
+    <></>
+  );
+};
+export { StackedAlert };
 
-render only a <AlertContainer /> in the <Alert/> whenever message is not empty
-then in the <AlertContainer /> add the setTimeout on a useffect only on onMount
-Also, add a cleanup to clearTimeout in the AlertContainer/>
-*/
+const AlertBody = ({ id, message, severity, iteration }) => {
+  // redux
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    let interval = setTimeout(() => {
+      dispatch(filterMessages(id));
+    }, 2500 + iteration * 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  const themes = {
+    ERROR: { border: "border-red-300", icon: "bg-red-100 text-red-800" }, // red
+    SUCCESS: {
+      border: "border-green-300",
+      icon: "bg-green-100 text-green-800",
+    }, // green
+    INFORMATION: {
+      border: "border-blue-300",
+      icon: "bg-blue-100 text-blue-800",
+    }, // blue
+    WARNING: {
+      border: "border-yellow-300",
+      icon: "bg-yellow-100 text-yellow-800",
+    }, // yellow
+  };
+
+  const icon = {
+    ERROR: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className="h-5 w-5"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+        strokeWidth={2}
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+        />
+      </svg>
+    ),
+    SUCCESS: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className="h-5 w-5"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+        strokeWidth={2}
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+        />
+      </svg>
+    ),
+    INFORMATION: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className="h-5 w-5"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+        strokeWidth={2}
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+        />
+      </svg>
+    ),
+    WARNING: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className="h-5 w-5"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+        strokeWidth={2}
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+        />
+      </svg>
+    ),
+  };
+
+  return (
+    <div
+      className={`bg-gray-50 h-8 w-60 
+        flex items-center gap-2
+        border rounded shadow-md
+        ${themes[severity.toUpperCase()].border}`}
+    >
+      <div
+        className={`${themes[severity.toUpperCase()].icon}  h-full
+         rounded-l p-2
+         flex items-center justify-center`}
+      >
+        {icon[severity.toUpperCase()]}
+      </div>
+
+      <p className="flex-grow text-sm font-semibold text-gray-800">{message}</p>
+    </div>
+  );
+};
