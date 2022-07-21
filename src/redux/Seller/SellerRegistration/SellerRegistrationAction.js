fe39bda4eaff881acd90b510a0230c182932df12
gn-grc <genejogarcia.gg@gmail.com>
2022-07-21 22:42:50 +0800

    removed steps-related states in seller registration

--- src/redux/Seller/SellerRegistration/SellerRegistrationAction.js
+++ src/redux/Seller/SellerRegistration/SellerRegistrationAction.js
@@ -2,8 +2,6 @@ const sellerRegistrationActionTypes = {
   AGREE_TO_TERMS_OF_AGREEMENT: "AGREE_TO_TERMS_OF_AGREEMENT",
   LOAD_ACCOUNT_DETAILS: "LOAD_ACCOUNT_DETAILS",
   LOAD_BUSINESS_DETAILS: "LOAD_BUSINESS_DETAILS",
-  CHANGE_CURRENT_STEP: "CHANGE_CURRENT_STEP",
-  PROCEED_TO_NEXT_STEP: "PROCEED_TO_NEXT_STEP",
   RESET_TO_DEFAULT: "RESET_TO_DEFAULT",
 };
 
@@ -25,20 +23,6 @@ const loadBusinessDetails = (businessDetails) => {
   };
 };
 
-const changeCurrentStep = (registrationStepId) => {
-  return {
-    type: sellerRegistrationActionTypes.CHANGE_CURRENT_STEP,
-    payload: registrationStepId,
-  };
-};
-
-const proceedToNextStep = (nextRegistrationStepId) => {
-  return {
-    type: sellerRegistrationActionTypes.PROCEED_TO_NEXT_STEP,
-    payload: nextRegistrationStepId,
-  };
-};
-
 const resetToDefault = () => {
   return {
     type: sellerRegistrationActionTypes.RESET_TO_DEFAULT,
@@ -50,7 +34,5 @@ export {
   agreeToTOA,
   loadAccountDetails,
   loadBusinessDetails,
-  changeCurrentStep,
-  proceedToNextStep,
   resetToDefault,
 };
