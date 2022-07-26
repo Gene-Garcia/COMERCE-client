dd10b53cace351c7fbc9c169ec18abee648ee011
gn-grc <genejogarcia.gg@gmail.com>
2022-07-26 22:39:30 +0800

    implemented tailwind dropdown in account

--- src/shared/Components/navigation/Account.jsx
+++ src/shared/Components/navigation/Account.jsx
@@ -16,10 +16,12 @@
  *
  */
 
-import React from "react";
+import { Menu, Transition } from "@headlessui/react";
+import React, { Fragment } from "react";
 import { Link } from "react-router-dom";
 import Cookies from "universal-cookie";
 import { checkLoggedIn } from "../../Auth/Login";
+import accountLinks from "./accountLinks";
 import Cart from "./Cart";
 
 function UnAuthenticated({ bgType }) {
@@ -53,19 +55,17 @@ function UnAuthenticated({ bgType }) {
   );
 }
 
-function Authenticated({ bgType }) {
+function Authenticated() {
   const cookies = new Cookies();
 
-  // const color = "text-white";
-  const themes = {
-    accent: "text-gray-50",
-    contrast: "text-accent",
-  };
-
   return (
-    <div className="self-center group dropdown inline-block relative px-3">
+    <Menu as="div" className="px-3">
       {/* username and the arrow */}
-      <button className={`${themes[bgType]} inline-flex items-center gap-0.5`}>
+      <Menu.Button
+        className={`text-stone-800 flex items-center gap-0.5
+                    transition duration-200 ease-linear
+                    group-hover:text-accent`}
+      >
         <span className="font-semibold text-sm">
           {cookies.get(process.env.REACT_APP_LS_USERNAME_KEY)}
         </span>
@@ -76,76 +76,57 @@ function Authenticated({ bgType }) {
         >
           <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
         </svg>
-      </button>
+      </Menu.Button>
 
-      {/* the menu */}
-      <ul className="w-64 dropdown-menu absolute -right-6 xs:right-0 z-50 hidden transition duration-300 ease-linear group-hover:block">
-        <div className="w-full mt-1 bg-white shadow-lg rounded border border-gray-200 px-2 py-4 space-y-1.5">
-          <li className="px-2">
-            <p className="text-xs font-regular text-gray-300">Signed in as</p>
-            <p className="text-sm font-semibold text-gray-500">
-              {cookies.get(process.env.REACT_APP_LS_EMAIL_KEY)}
-            </p>
-          </li>
-
-          <AuthenticatedLink
-            to="/user"
-            name="My Account"
-            svg="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z"
-          />
-
-          <hr />
-
-          <AuthenticatedLink
-            to="/user/purchases"
-            name="Purchases"
-            svg="M13 7h8m0 0v8m0-8l-8 8-4-4-6 6"
-          />
-
-          <AuthenticatedLink
-            to="/user/orders"
-            name="Orders"
-            svg="M13 16V6a1 1 0 00-1-1H4a1 1 0 00-1 1v10a1 1 0 001 1h1m8-1a1 1 0 01-1 1H9m4-1V8a1 1 0 011-1h2.586a1 1 0 01.707.293l3.414 3.414a1 1 0 01.293.707V16a1 1 0 01-1 1h-1m-6-1a1 1 0 001 1h1M5 17a2 2 0 104 0m-4 0a2 2 0 114 0m6 0a2 2 0 104 0m-4 0a2 2 0 114 0"
-          />
-
-          <hr />
-
-          <AuthenticatedLink
-            to="/sign-out"
-            name="Sign Out"
-            svg="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"
-          />
-        </div>
-      </ul>
-    </div>
-  );
-}
-
-// a component to render the link for the user dropdown menu
-function AuthenticatedLink({ to, name, svg }) {
-  return (
-    <li className="rounded transition ease-linear hover:bg-gray-100 px-2 py-1">
-      <Link
-        className="flex flex-row items-center gap-3 font-medium text-gray-500"
-        to={to}
+      <Transition
+        as={Fragment}
+        enter="transition ease-out duration-100"
+        enterFrom="transform opacity-0 scale-95"
+        enterTo="transform opacity-100 scale-100"
+        leave="transition ease-in duration-75"
+        leaveFrom="transform opacity-100 scale-100"
+        leaveTo="transform opacity-0 scale-95"
       >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="h-6 w-6"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
+        {/* the menu */}
+        <Menu.Items
+          className="z-50 absolute right-8 mt-2 h-full w-56 origin-top-right 
+                    focus:outline-none"
         >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth={2}
-            d={svg}
-          />
-        </svg>
-        {name}
-      </Link>
-    </li>
+          <div className="bg-white rounded-md shadow-lg border-b">
+            <div className="px-2.5 py-2.5 border-b border-stone-300">
+              <p className="text-xs font-regular text-gray-300">Signed in as</p>
+              <p className="text-sm font-semibold text-gray-500">
+                {cookies.get(process.env.REACT_APP_LS_EMAIL_KEY)}
+              </p>
+            </div>
+
+            <div className="">
+              {accountLinks.map((group) => (
+                <div className="border-b border-stone-300 py-2 space-y-0.5">
+                  {group.map(({ id, to, name, svg }) => (
+                    <Menu.Item key={id}>
+                      {({ active }) => (
+                        <Link
+                          className="flex flex-row items-center gap-3 
+                                  py-1.5 px-3
+                                  text-stone-500
+                                  transition duration-200 ease-linear 
+                                  hover:bg-neutral-200 hover:text-stone-800"
+                          to={to}
+                        >
+                          {svg}
+                          <span className="font-medium text-sm">{name}</span>
+                        </Link>
+                      )}
+                    </Menu.Item>
+                  ))}
+                </div>
+              ))}
+            </div>
+          </div>
+        </Menu.Items>
+      </Transition>
+    </Menu>
   );
 }
 
